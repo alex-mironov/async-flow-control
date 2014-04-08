ad8d6f8515d3ecfea9ff5894a577839e24aea8bd
alex-mironov <alexander.myronov@gmail.com>
2014-04-08 15:11:40 +0300

    logs have been changed for first and second methods

--- sequential-example.js
+++ sequential-example.js
@@ -3,13 +3,13 @@ var sequential = require('./sequential');
 sequential([
 	function (callback) {
 		setTimeout(function () {
-			console.log('first task completed');
-			callback(null, 'passed from the first task');
+			console.log('1st task completed');
+			callback(null, 'passed from the 1st task');
 		}, 500);
 	}, function (callback) {
 		setTimeout(function () {
-			console.log('second task completed');
-			callback(null, 'passed from the second task');
+			console.log('2nd task completed');
+			callback(null, 'passed from the 2nd task');
 		}, 300);
 	}, function (callback) {
 		setTimeout(function () {
